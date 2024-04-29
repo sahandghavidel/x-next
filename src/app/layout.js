ffd85c80741ae59d76d3cdd718bcc9670ed6a9f9
Sahand Ghavidel <sahand.gha@gmail.com>
2024-04-29 12:24:29 +0700

    Modify the sidebar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body className={inter.className}>
         <div className='flex justify-between max-w-6xl mx-auto'>
-          <div>
+          <div className='hidden sm:inline border-r h-screen'>
             <Sidebar />
           </div>
           <div>{children}</div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+import { FaXTwitter } from 'react-icons/fa6';
+import { HiHome } from 'react-icons/hi';
+
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className='flex flex-col gap-4 p-3'>
+      <Link href='/'>
+        <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
+      </Link>
+      <Link
+        href='/'
+        className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
+      >
+        <HiHome className='w-7 h-7' />
+        <span className='font-bold hidden xl:inline'>Home</span>
+      </Link>
+      <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>
+        Sign In
+      </button>
+    </div>
+  );
 }
