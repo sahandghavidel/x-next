1420a4b0c3fa614dd368bbe7cc4e9defd4c7d295
Sahand Ghavidel <sahand.gha@gmail.com>
2024-04-29 18:00:35 +0700

    Update the News component

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,14 @@ export default function RootLayout({ children }) {
             <Sidebar />
           </div>
           <div>{children}</div>
-          <div>
+          <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
+            <div className='sticky top-0 bg-white py-2'>
+              <input
+                type='text'
+                placeholder='Search'
+                className='bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2'
+              ></input>
+            </div>
             <News />
           </div>
         </div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,41 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export default function News() {
+  const [news, setNews] = useState([]);
+  const [articleNum, setArticleNum] = useState(3);
+  useEffect(() => {
+    fetch('https://saurav.tech/NewsAPI/top-headlines/category/business/us.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data.articles);
+      });
+  }, []);
   return (
-    <div>News</div>
-  )
+    <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2'>
+      <h4 className='font-bold text-xl px-4'>Whats happening</h4>
+      {news.slice(0, articleNum).map((article) => (
+        <div key={article.url}>
+          <a href={article.url} target='_blank'>
+            <div className='flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200'>
+              <div className='space-y-0.5'>
+                <h6 className='text-sm font-bold'>{article.title}</h6>
+                <p className='text-xs font-medium text-gray-500'>
+                  {article.source.name}
+                </p>
+              </div>
+              <img src={article.urlToImage} width={70} className='rounded-xl' />
+            </div>
+          </a>
+        </div>
+      ))}
+      <button
+        onClick={() => setArticleNum(articleNum + 3)}
+        className='text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm'
+      >
+        Load more
+      </button>
+    </div>
+  );
 }
