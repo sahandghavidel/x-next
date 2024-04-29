832d71e2c0173e25282012e7cedc257f53258c38
Sahand Ghavidel <sahand.gha@gmail.com>
2024-04-29 12:25:36 +0700

    Update font weight of Sign In button in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar() {
         <HiHome className='w-7 h-7' />
         <span className='font-bold hidden xl:inline'>Home</span>
       </Link>
-      <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>
+      <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
         Sign In
       </button>
     </div>
