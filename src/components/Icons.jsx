b866307ae1108564fecb472698c44b37a5f739e5
Sahand Ghavidel <sahand.gha@gmail.com>
2024-05-03 11:49:15 +0700

    Update Icons component to handle likes

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,12 +1,79 @@
 'use client';
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash } from 'react-icons/hi';
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from 'react-icons/hi';
 
-export default function Icons() {
+import { signIn, useSession } from 'next-auth/react';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+} from 'firebase/firestore';
+import { app } from '../firebase';
+import { useEffect, useState } from 'react';
+
+export default function Icons({ id }) {
+  const { data: session } = useSession();
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState([]); // [1
+  const db = getFirestore(app);
+  const likePost = async () => {
+    if (session) {
+      if (isLiked) {
+        await deleteDoc(doc(db, 'posts', id, 'likes', session?.user.uid));
+      } else {
+        await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+          username: session.user.username,
+          timestamp: serverTimestamp(),
+        });
+      }
+    } else {
+      signIn();
+    }
+  };
+
+  useEffect(() => {
+    onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) => {
+      setLikes(snapshot.docs);
+    });
+  }, [db]);
+
+  useEffect(() => {
+    setIsLiked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    );
+  }, [likes]);
+
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
       <HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100' />
-      <HiOutlineHeart className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
+      <div className='flex items-center'>
+        {isLiked ? (
+          <HiHeart
+            onClick={likePost}
+            className='h-8 w-8 cursor-pointer rounded-full  transition text-red-600 duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+          />
+        ) : (
+          <HiOutlineHeart
+            onClick={likePost}
+            className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+          />
+        )}
+        {likes.length > 0 && (
+          <span className={`text-xs ${isLiked && 'text-red-600'}`}>
+            {likes.length}
+          </span>
+        )}
+      </div>
+
       <HiOutlineTrash className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
     </div>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,7 +13,7 @@ export default function Post({ post, id }) {
       <div className='flex-1'>
         <div className='flex items-center justify-between'>
           <div className='flex items-center space-x-1 whitespace-nowrap'>
-            <h4 className='font-bold text-sx truncate'>{post?.name}</h4>
+            <h4 className='font-bold text-sm truncate'>{post?.name}</h4>
             <span className='text-xs truncate'>@{post?.username}</span>
           </div>
           <HiDotsHorizontal className='text-sm' />
@@ -25,7 +25,7 @@ export default function Post({ post, id }) {
         <Link href={`/posts/${id}`}>
           <img src={post?.image} className='rounded-2xl mr-2' />
         </Link>
-        <Icons />
+        <Icons id={id} />
       </div>
     </div>
   );
