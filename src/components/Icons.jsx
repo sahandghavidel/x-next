32df138fa06b03b47b1be03345861b72c534f595
Sahand Ghavidel <sahand.gha@gmail.com>
2024-05-04 11:43:58 +0700

    Update deletePost function in Icons component to handle post deletion

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -20,7 +20,7 @@ import {
 import { app } from '../firebase';
 import { useEffect, useState } from 'react';
 
-export default function Icons({ id }) {
+export default function Icons({ id, uid }) {
   const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]); // [1
@@ -52,6 +52,23 @@ export default function Icons({ id }) {
     );
   }, [likes]);
 
+  const deletePost = async () => {
+    if (window.confirm('Are you sure you want to delete this post?')) {
+      if (session?.user?.uid === uid) {
+        deleteDoc(doc(db, 'posts', id))
+          .then(() => {
+            console.log('Document successfully deleted!');
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error('Error removing document: ', error);
+          });
+      } else {
+        alert('You are not authorized to delete this post');
+      }
+    }
+  };
+
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
       <HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100' />
@@ -74,7 +91,12 @@ export default function Icons({ id }) {
         )}
       </div>
 
-      <HiOutlineTrash className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
+      {session?.user?.uid === uid && (
+        <HiOutlineTrash
+          className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+          onClick={deletePost}
+        />
+      )}
     </div>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,7 +25,7 @@ export default function Post({ post, id }) {
         <Link href={`/posts/${id}`}>
           <img src={post?.image} className='rounded-2xl mr-2' />
         </Link>
-        <Icons id={id} />
+        <Icons id={id} uid={post.uid} />
       </div>
     </div>
   );
